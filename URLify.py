213093a4153e94479256d109e09b6f60d4731340
Mayank <myk.vit@gmail.com>
2020-09-29 20:30:40 -0500

    refactored code for this problem

--- URLify.py
+++ URLify.py
@@ -7,15 +7,13 @@
 # Output: "Mr%203ohn%20Smith
 
 class Solution(object):
-    def urlify(self,s):
+    def urlify(self, s):
         words = s.split()
-        url= ''
-        pointer = 0
+        url = ''
         for x in words:
-            if pointer < len(words)-1:
-                url = url + x + "%20"
-                pointer+=1
-            else:
-                return url
+            url = url + x + "%20"
+        return url
+
+
 obj = Solution()
 print(obj.urlify("myk ydv jonh wick jason free human                 "))
