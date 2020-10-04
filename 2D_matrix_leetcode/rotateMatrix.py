19bef07163c4c466a4d8b550b359adcb04ffc8b4
Mayank <myk.vit@gmail.com>
2020-10-04 14:46:46 -0500

    solution for rotate matrix

---
+++ 2D_matrix_leetcode/rotateMatrix.py
@@ -0,0 +1,32 @@
+# Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
+# bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+# Given input matrix =
+# [
+#   [ 5, 1, 9,11],
+#   [ 2, 4, 8,10],
+#   [13, 3, 6, 7],
+#   [15,14,12,16]
+# ],
+#
+# rotate the input matrix in-place such that it becomes:
+# [
+#   [15,13, 2, 5],
+#   [14, 3, 4, 1],
+#   [12, 6, 8, 9],
+#   [16, 7,10,11]
+# ]
+class Solution(object):
+    def rotateMatrix(self,matrix):
+        matrix.reverse()
+        print(matrix)
+        for i in range(len(matrix)):
+            for j in range(i+1):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        return matrix
+
+obj = Solution()
+print(obj.rotateMatrix([
+[1,2,3],
+[4,5,6],
+[7,8,9]]
+))
