d6c848c00d5840d46e0d15bd1d1c83be6b8e47c0
Mayank <myk.vit@gmail.com>
2020-10-11 22:15:26 -0500

    preorder traversal leetcode

---
+++ Tree Traversal/preOrderTraversal.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def preorderTraversal(self, root):
+        res = [] 
+        stack = [root]
+        
+        if not root:
+            return None
+        
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
