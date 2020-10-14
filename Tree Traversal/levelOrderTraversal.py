65772d80275814d9bb06f6c989055462855a686e
Mayank <myk.vit@gmail.com>
2020-10-13 23:19:42 -0500

    solution for level order traversal leetcode

---
+++ Tree Traversal/levelOrderTraversal.py
@@ -0,0 +1,26 @@
+
+# from right to left , level by level
+
+from collections import deque
+class Solution(object):
+    def levelOrder(self,root):
+        """
+         :type root: TreeNode
+         :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        queue, res = deque([root]), []
+        
+        while queue:
+            currLevel = []
+            size = len(queue)
+            for x in range(0,size):
+                node = queue.popleft()
+                if node and node.left:
+                    queue.append(node.left)
+                if node and node.right:
+                    queue.append(node.right)
+                currLevel.append(node.val)
+            res.append(currLevel)
+        return res
