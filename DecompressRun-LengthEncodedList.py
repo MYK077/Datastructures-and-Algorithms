cd0c6e249c5e51ffa5080d81aed56297c85a3cd4
Mayank <myk.vit@gmail.com>
2020-10-16 08:59:04 -0500

    solution for decompress leetcode

---
+++ DecompressRun-LengthEncodedList.py
@@ -0,0 +1,9 @@
+
+
+
+class Solution:
+    def decompressRLElist(self, nums):
+        result = []
+        for i in range(0,len(nums),2):
+            result.extend([nums[i+1]]*nums[i])
+        return result
